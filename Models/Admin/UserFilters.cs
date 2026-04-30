40f48e95726c49d1b01bd9fe500d6ed6d076fdec
Percival-21Code <charlesapercival.1@gmail.com>
2026-04-30 15:17:22 +0100

    Add admin user read endpoints

---
+++ Models/Admin/UserFilters.cs
@@ -0,0 +1,10 @@
+﻿namespace mysystem_user_api.Models.Admin;
+
+public class UserFilters
+{
+    public string? UserId { get; set; }
+    public string? Role { get; set; }
+    public string? CustomerNo { get; set; }
+    public string? SiteId { get; set; }
+    public bool? IsActive { get; set; }
+}
